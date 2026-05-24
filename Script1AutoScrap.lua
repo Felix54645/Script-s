4c41fc0cf02f2c16ff8a19f6baa90cc24967853f
Felix54645 <kamalyantigrlev@gmail.com>
2026-05-24 20:52:29 +0500

    Update Script1AutoScrap.lua

--- Script1AutoScrap.lua
+++ Script1AutoScrap.lua
@@ -1,4 +1,1 @@
-local PlayerGui = game.Players.LocalPlayer.PlayerGui
-
-local Frame = Instance.new("Frame")
-Frame.Parent = PlayerGui
+https://yandex.ru/search/?text=loadstring(game%3AHttpGet(%22https%3A%2F%2Fpastebin.com%2Fraw%2FRCrjGr7h%22))()&lr=11177&clid=2270455&win=724
